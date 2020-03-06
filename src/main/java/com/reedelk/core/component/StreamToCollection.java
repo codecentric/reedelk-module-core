fc1331d1305bca140e50feeb322988400ea8d46f
lorenzo <lorenzo@lorenzo.info.com>
2020-03-06 13:58:23 +0100

    code

--- src/main/java/com/reedelk/core/component/StreamToCollection.java
+++ src/main/java/com/reedelk/core/component/StreamToCollection.java
@@ -2,16 +2,17 @@ package com.reedelk.core.component;
 
 import com.reedelk.runtime.api.annotation.Description;
 import com.reedelk.runtime.api.annotation.ModuleComponent;
-import com.reedelk.runtime.api.commons.StreamUtils;
 import com.reedelk.runtime.api.component.ProcessorSync;
 import com.reedelk.runtime.api.flow.FlowContext;
 import com.reedelk.runtime.api.message.Message;
 import com.reedelk.runtime.api.message.MessageBuilder;
+import com.reedelk.runtime.api.message.content.TypedContent;
 import org.osgi.service.component.annotations.Component;
 
 import java.util.List;
 
 import static com.reedelk.runtime.api.commons.Preconditions.checkArgument;
+import static com.reedelk.runtime.api.commons.StreamUtils.FromObject;
 import static org.osgi.service.component.annotations.ServiceScope.PROTOTYPE;
 
 @ModuleComponent("Stream To Collection")
@@ -23,10 +24,11 @@ public class StreamToCollection implements ProcessorSync {
 
     @Override
     public Message apply(FlowContext flowContext, Message message) {
-        checkArgument(message.content().isStream(), "Expected payload containing a stream but it was not.");
+        TypedContent<Object, Object> content = message.content();
+        checkArgument(content.isStream(), "Expected payload containing a stream but it was not.");
+        checkArgument(!content.isConsumed(), "Expected payload containing a stream not consumed but it was not.");
 
-        List<Object> streamAsList =
-                StreamUtils.FromObject.consume(message.content().stream());
+        List<Object> streamAsList = FromObject.consume(content.stream());
 
         return MessageBuilder.get()
                 .attributes(message.getAttributes())
