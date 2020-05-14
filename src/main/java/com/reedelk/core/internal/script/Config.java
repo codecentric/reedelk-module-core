51223b13fd93e3f660888b3ff8125dadb0cb6ea1
lorenzo <info@reedelk.com>
2020-05-14 11:08:24 +0200

    @Annotations

--- src/main/java/com/reedelk/core/internal/script/Config.java
+++ src/main/java/com/reedelk/core/internal/script/Config.java
@@ -4,9 +4,7 @@ import com.reedelk.runtime.api.annotation.Type;
 import com.reedelk.runtime.api.annotation.TypeFunction;
 import com.reedelk.runtime.api.configuration.ConfigurationService;
 
-// TODO: Important! Switching to groovy there is no more constraint in having different
-//   names for different methods with different arguments. These methods and all the overloads
-//      must be made uniform: e.g: asString(String configKey) and asStringWithDefault(String configKey, String defaultValue).
+
 @Type(global = true, description =
         "The Config type provides a set of functions to retrieve " +
                 "configuration properties given a config key and (optionally) a default value. " +
@@ -24,7 +22,7 @@ public class Config {
 
     @TypeFunction(
             cursorOffset = 1,
-            signature = "asString(configKey: string)",
+            signature = "asString(String configKey)",
             example = "Config.asString('endpoint.host')",
             description = "Returns the configuration value of the given config key as String type.")
     public String asString(String configKey) {
@@ -33,11 +31,11 @@ public class Config {
 
     @TypeFunction(
             cursorOffset = 1,
-            signature = "asStringWithDefault(configKey: string, defaultValue: string)",
-            example = "Config.asStringWithDefault('endpoint.host', 'localhost')",
+            signature = "asString(String configKey, String defaultValue)",
+            example = "Config.asString('endpoint.host', 'localhost')",
             description = "Returns the configuration value of the given config key as a String type if present, " +
                     "otherwise the given default value is returned.")
-    public String asStringWithDefault(String configKey, String defaultValue) {
+    public String asString(String configKey, String defaultValue) {
         return configurationService.getString(configKey, defaultValue);
     }
 
@@ -45,7 +43,7 @@ public class Config {
 
     @TypeFunction(
             cursorOffset = 1,
-            signature = "asInt(configKey: string)",
+            signature = "asInt(String configKey)",
             example = "Config.asInt('endpoint.port')",
             description = "Returns the configuration value of the given config key as int type.")
     public int asInt(String configKey) {
@@ -54,11 +52,11 @@ public class Config {
 
     @TypeFunction(
             cursorOffset = 1,
-            signature = "asIntWithDefault(configKey: string, defaultValue: int)",
-            example = "Config.asIntWithDefault('endpoint.port', 8282)",
+            signature = "asInt(String configKey, int defaultValue)",
+            example = "Config.asInt('endpoint.port', 8282)",
             description = "Returns the configuration value of the given config key as a int type if present, " +
                     "otherwise the given default value is returned.")
-    public int asIntWithDefault(String configKey, int defaultValue) {
+    public int asInt(String configKey, int defaultValue) {
         return configurationService.getInt(configKey, defaultValue);
     }
 
@@ -66,7 +64,7 @@ public class Config {
 
     @TypeFunction(
             cursorOffset = 1,
-            signature = "asLong(configKey: string)",
+            signature = "asLong(String configKey)",
             example = "Config.asLong('my.config.property')",
             description = "Returns the configuration value of the given config key as long type.")
     public long asLong(String configKey) {
@@ -75,11 +73,11 @@ public class Config {
 
     @TypeFunction(
             cursorOffset = 1,
-            signature = "asLongWithDefault(configKey: string, defaultValue: long)",
-            example = "Config.asLongWithDefault('my.config.property', 540221)",
+            signature = "asLong(String configKey, long defaultValue)",
+            example = "Config.asLong('my.config.property', 540221)",
             description = "Returns the configuration value of the given config key as a long type if present, " +
                     "otherwise the given default value is returned.")
-    public long asLongWithDefault(String configKey, long defaultValue) {
+    public long asLong(String configKey, long defaultValue) {
         return configurationService.getLong(configKey, defaultValue);
     }
 
@@ -87,7 +85,7 @@ public class Config {
 
     @TypeFunction(
             cursorOffset = 1,
-            signature = "asDouble(configKey: string)",
+            signature = "asDouble(String configKey)",
             example = "Config.asDouble('my.config.property')",
             description = "Returns the configuration value of the given config key as double type.")
     public double asDouble(String configKey) {
@@ -96,11 +94,11 @@ public class Config {
 
     @TypeFunction(
             cursorOffset = 1,
-            signature = "asDoubleWithDefault(configKey: string, defaultValue: double)",
-            example = "Config.asDoubleWithDefault('my.config.property', 21.7823)",
+            signature = "asDouble(String configKey, double defaultValue)",
+            example = "Config.asDouble('my.config.property', 21.7823)",
             description = "Returns the configuration value of the given config key as a double type if present, " +
                     "otherwise the given default value is returned.")
-    public double asDoubleWithDefault(String configKey, double defaultValue) {
+    public double asDouble(String configKey, double defaultValue) {
         return configurationService.getDouble(configKey, defaultValue);
     }
 
@@ -108,7 +106,7 @@ public class Config {
 
     @TypeFunction(
             cursorOffset = 1,
-            signature = "asFloat(configKey: string)",
+            signature = "asFloat(String configKey)",
             example = "Config.asFloat('my.config.property')",
             description = "Returns the configuration value of the given config key as float type.")
     public float asFloat(String configKey) {
@@ -117,11 +115,11 @@ public class Config {
 
     @TypeFunction(
             cursorOffset = 1,
-            signature = "asFloatWithDefault(configKey: string, defaultValue: float)",
-            example = "Config.asFloatWithDefault('my.config.property', 3.1)",
+            signature = "asFloat(String configKey, float defaultValue)",
+            example = "Config.asFloat('my.config.property', 3.1)",
             description = "Returns the configuration value of the given config key as a float type if present, " +
                     "otherwise the given default value is returned.")
-    public float asFloatWithDefault(String configKey, float defaultValue) {
+    public float asFloat(String configKey, float defaultValue) {
         return configurationService.getFloat(configKey, defaultValue);
     }
 
@@ -129,7 +127,7 @@ public class Config {
 
     @TypeFunction(
             cursorOffset = 1,
-            signature = "asBoolean(configKey: string)",
+            signature = "asBoolean(String configKey)",
             example = "Config.asBoolean('my.config.property')",
             description = "Returns the configuration value of the given config key as boolean type.")
     public boolean asBoolean(String configKey) {
@@ -138,11 +136,11 @@ public class Config {
 
     @TypeFunction(
             cursorOffset = 1,
-            signature = "asBooleanWithDefault(configKey: string, defaultValue: boolean)",
-            example = "Config.asBooleanWithDefault('my.config.property', true)",
+            signature = "asBoolean(String configKey, boolean defaultValue)",
+            example = "Config.asBoolean('my.config.property', true)",
             description = "Returns the configuration value of the given config key as a boolean type if present, " +
                     "otherwise the given default value is returned.")
-    public boolean asBooleanWithDefault(String configKey, boolean defaultValue) {
+    public boolean asBoolean(String configKey, boolean defaultValue) {
         return configurationService.getBoolean(configKey, defaultValue);
     }
 
